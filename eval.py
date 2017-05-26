- added softmax function - calculate the probability from the score using   softmax function and save it in prediction.csv file - fixed an issue with empty datasets["target_names"]

eval.py
@@ -3,14 +3,20 @@
 import tensorflow as tf
 import numpy as np
 import os
-import time
-import datetime
 import data_helpers
-from text_cnn import TextCNN
 from tensorflow.contrib import learn
 import csv
 from sklearn import metrics
 import yaml
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    if x.ndim == 1:
+        x = x.reshape((1, -1))
+    max_x = np.max(x, axis=1).reshape((-1, 1))
+    exp_x = np.exp(x - max_x)
+    return exp_x / np.sum(exp_x, axis=1).reshape((-1, 1))
 
 with open("config.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
@@ -55,9 +61,11 @@ if FLAGS.eval_train:
     print("Total number of test examples: {}".format(len(y_test)))
 else:
     if dataset_name == "mrpolarity":
+        datasets = {"target_names": ['positive_examples', 'negative_examples']}
         x_raw = ["a masterpiece four years in the making", "everything is off."]
         y_test = [1, 0]
     else:
+        datasets = {"target_names": ['alt.atheism', 'comp.graphics', 'sci.med', 'soc.religion.christian']}
         x_raw = ["The number of reported cases of gonorrhea in Colorado increased",
                  "I am in the market for a 24-bit graphics card for a PC"]
         y_test = [2, 1]
@@ -89,6 +97,9 @@ with graph.as_default():
         dropout_keep_prob = graph.get_operation_by_name("dropout_keep_prob").outputs[0]
 
         # Tensors we want to evaluate
+        scores = graph.get_operation_by_name("output/scores").outputs[0]
+
+        # Tensors we want to evaluate
         predictions = graph.get_operation_by_name("output/predictions").outputs[0]
 
         # Generate batches for one epoch
@@ -96,10 +107,16 @@ with graph.as_default():
 
         # Collect the predictions here
         all_predictions = []
+        all_probabilities = None
 
         for x_test_batch in batches:
-            batch_predictions = sess.run(predictions, {input_x: x_test_batch, dropout_keep_prob: 1.0})
-            all_predictions = np.concatenate([all_predictions, batch_predictions])
+            batch_predictions_scores = sess.run([predictions, scores], {input_x: x_test_batch, dropout_keep_prob: 1.0})
+            all_predictions = np.concatenate([all_predictions, batch_predictions_scores[0]])
+            probabilities = softmax(batch_predictions_scores[1])
+            if all_probabilities is not None:
+                all_probabilities = np.concatenate([all_probabilities, probabilities])
+            else:
+                all_probabilities = probabilities
 
 # Print accuracy if y_test is defined
 if y_test is not None:
@@ -110,7 +127,9 @@ if y_test is not None:
     print(metrics.confusion_matrix(y_test, all_predictions))
 
 # Save the evaluation to a csv
-predictions_human_readable = np.column_stack((np.array(x_raw), all_predictions))
+predictions_human_readable = np.column_stack((np.array(x_raw),
+                                              [int(prediction) for prediction in all_predictions],
+                                              [ "{}".format(probability) for probability in all_probabilities]))
 out_path = os.path.join(FLAGS.checkpoint_dir, "..", "prediction.csv")
 print("Saving evaluation to {0}".format(out_path))
 with open(out_path, 'w') as f: